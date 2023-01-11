db0e2376050bed717135fa2b9547e1d5fdfef87c
Alexsandro <alexsandrojunior53@gmail.com>
2023-01-11 19:59:02 -0300

    js criado

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const ratings = document.querySelectorAll('.label');
+const card = document.querySelector('.card');
+const cardBack = document.querySelector('.back-side');
+const ratingMessage = document.querySelector('.rating-selected');
+const radio = document.querySelector('.radio');
+
+document.querySelector('.form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    // if () {
+    //     card.classList.add("hidden")
+    //     cardBack.classList.remove('hidden')
+    //     ratingMessage.innerHTML = '<h2>You selected 4 out of 5</h2>'
+    // } else {
+        
+    // }
+})
