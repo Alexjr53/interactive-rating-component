5bcedd448791e86f1d09cc35e5ebd685d1af14db
Alexsandro <alexsandrojunior53@gmail.com>
2023-01-13 17:35:40 -0300

    js finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,19 @@
-const ratings = document.querySelectorAll('.label');
+const ratings = document.querySelectorAll('.ratings-btn');
 const card = document.querySelector('.card');
-const cardBack = document.querySelector('.back-side');
+const cardBack = document.querySelector('.back-side-card');
 const ratingMessage = document.getElementById('rating-selected');
 const form = document.querySelector('.form')
+const errorMessage = document.querySelector('.error-message')
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
     if (ratingMessage.textContent === "") {
-      alert('olá')
+      errorMessage.innerHTML = 'Please select feedback rating before submitting *'
+      errorMessage.style.display = 'block'
     } else {
       cardBack.classList.remove('hidden');
       card.classList.add('hidden');
+      errorMessage.style.display = 'none'
     }
   });
 
